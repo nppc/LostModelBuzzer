927c843f3a5ae87ebf4683e1703b3fd341fd39cb
Pavel Palonen <pavel.palonen@gmail.com>
2016-09-23 15:51:18 +0200

    Create Configure.ino

---
+++ Configure.ino
@@ -0,0 +1,44 @@
+#define OUT_PIN  10
+String inString = "";
+unsigned int intNum;
+
+void setup() {
+  pinMode(OUT_PIN, OUTPUT);
+  digitalWrite(OUT_PIN, HIGH);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+	//wait for text numeric value
+	while (Serial.available() > 0) {
+		int inChar = Serial.read();
+		if(isDigit(inChar)) {
+			inString += (char)inChar;
+		}
+		if(inChar=='\n') {
+			//Data is ready, lets send it...
+			intNum = inString.toInt();
+			sendNum(intNum);
+		}
+    }
+}
+
+void sendNum(unsigned int nb) {
+	digitalWrite(OUT_PIN, HIGH);
+	delay(50);
+	byte bitLen;
+	for(byte i=0;i<16;i++) {
+		if((nb >> i) & 1)
+		    bitLen=9;
+		else
+		    bitLen=4;
+		digitalWrite(OUT_PIN, LOW);
+		delay(1);
+		digitalWrite(OUT_PIN, HIGH);
+		delay(bitLen);
+	}
+	digitalWrite(OUT_PIN, LOW);
+	delay(1);
+	digitalWrite(OUT_PIN, HIGH);
+}
